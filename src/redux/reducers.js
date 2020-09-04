96ac5d288337b7ed600c8a7178a7b696becc7c79
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-09-03 20:36:30 -0500

    everything seems to be working

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,6 +4,14 @@ const user = (state = null) => state
 
 // add switch statements in here
 const cars = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_CAR': 
+        return [...state, action.payload];
+        case 'REMOVE_CAR': 
+        const newState = [...state]
+        newState.splice(action.value, 1)
+        return newState;
+    }
     return state
 }
 

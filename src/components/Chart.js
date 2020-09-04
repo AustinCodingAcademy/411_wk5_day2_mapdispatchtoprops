bfbb1cb1bb722eb85b193cadd11486cde8491ea4
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-09-03 20:38:40 -0500

    removed unneed instructions

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,8 +2,6 @@ import React from 'react'
 import PieChart from 'react-minimal-pie-chart';
 
 const Chart = (props) => {
-    // create variable "over" with all the cars whos horsepower is >= 200
-    // create variable "under" with all the cars whos horsepower is < 200
    const over = props.cars.filter(car => car.horsepower >= 200)
    const under = props.cars.filter(car => car.horsepower < 200)
 

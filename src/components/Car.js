83d9bf4536edd8955930c15512a345c9d16fda96
patfitz95 <patfitz95@gmail.com>
2020-09-04 14:53:25 -0500

    homework completed

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { Container, Paper, Chip } from '@material-ui/core';
-import cars from '../cars.json' // remove this
 
 const Car = (props) => {
     const id = props.match.params.id
-    {/* Change cars to props.cars and remove the cars.json import above */}
     const car = props.cars.find(c => c.id == id)
 
     return (
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,7 +4,15 @@ const user = (state = null) => state
 
 // add switch statements in here
 const cars = (state = [], action) => {
-    return state
+    switch(action.type) {
+        case 'ADD_CAR':
+            return [ ...state, action.value ]
+            default: return state
+        case 'REMOVE_CAR':
+            const newState = [ ...state ]
+            newState.splice(action.value, 1);
+            return newState;
+    }
 }
 
 export default combineReducers({ user, cars })
